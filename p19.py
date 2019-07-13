c3a82e49742aae4357ab6e6efe345fffd7f30c82
Aravind1145 <52447550+Aravind1145@users.noreply.github.com>
2019-07-13 10:57:24 +0530

    Create p19.py

---
+++ p19.py
@@ -0,0 +1,10 @@
+nnu=int(input())
+for i in range(2,nnu+1):
+  if(nnu%i==0):
+    t=0
+    for s in range(2,i+1):
+      if(i%s==0) and (s!=i):
+          t=1
+          break
+    if(t==0):
+          print(i,end=" ")
